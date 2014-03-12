d4f6331679390b98a419929783062d1d324611f1
Wade Wegner <wade.wegner@gmail.com>
2014-03-12 10:31:05 -0700

    Updated python code

---
+++ demo2/python/README.md
@@ -0,0 +1,8 @@
+# Setup demo
+
+Run the following commands from the directory:
+
+	virtualenv --no-site-packages python
+	source bin/activate
+
+	pip install -r requirements.txt
---
+++ demo2/python/run.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+consumer_key = ""
+consumer_secret = ""
+username = ""
+password = ""
+
+payload = {
+    'grant_type': 'password',
+    'client_id': consumer_key,
+    'client_secret': consumer_secret,
+    'username': username,
+    'password': password
+}
+
+r = requests.post("https://login.salesforce.com/services/oauth2/token", 
+    headers={"Content-Type":"application/x-www-form-urlencoded"},
+    data=payload)
+
+token = json.loads(r.content)
+
+access_token = token[u'access_token']
+instance_url = token[u'instance_url']
+
+print ""
+print "Access Token: " + access_token
+print "Instance URL: " + instance_url
+print ""
+
+object_name = "Widget__c"
+payload = {"Name":"New Widget for Python"}
+
+r = requests.post(instance_url + "/services/data/v29.0/sobjects/" + object_name,
+	headers={"Content-Type":"application/json",
+		"Authorization":"Bearer " + access_token},
+	data=json.dumps(payload))
+
+print r.content
