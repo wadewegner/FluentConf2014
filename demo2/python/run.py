7021d14c3b8286830694635e5e8aadff995174ce
Wade Wegner <wade.wegner@gmail.com>
2014-03-14 09:30:53 -0700

    Fixed deck

--- demo2/python/run.py
+++ demo2/python/run.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import base64
 
-consumer_key = ""
-consumer_secret = ""
-username = ""
-password = ""
+consumer_key = base64.b64decode("M01WRzlBMmtOM0JuMTdoc0V5TXFSVFRhRWZmWWN0ZG9oME1hMW03YlpGenJDZ0pzR2RHMHZGanEzcHlOMnBJMEFkSHVrX2RIQ1FFanZjLlFRWXBXWQ==")
+consumer_secret = base64.b64decode("NDIyMzM1MTQzNzYxMDg4ODIwNA==")
+username = base64.b64decode("ZGVtb0BzZmRjYXBpLmNvbQ==")
+password = base64.b64decode("UGEkJHcwcmQh")
 
 payload = {
     'grant_type': 'password',
@@ -19,7 +20,6 @@ r = requests.post("https://login.salesforce.com/services/oauth2/token",
     data=payload)
 
 token = json.loads(r.content)
-
 access_token = token[u'access_token']
 instance_url = token[u'instance_url']
 
@@ -29,7 +29,7 @@ print "Instance URL: " + instance_url
 print ""
 
 object_name = "Widget__c"
-payload = {"Name":"New Widget for Python"}
+payload = {"Name":"New Widget from Python"}
 
 r = requests.post(instance_url + "/services/data/v29.0/sobjects/" + object_name,
 	headers={"Content-Type":"application/json",
